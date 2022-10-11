f05ac4da18bc078f5fbed42b6608b17d6092cd03
AleksaGrri <aleksgrishenckova@yandex.com>
2022-10-11 16:26:42 +0300

    скоро финал

--- src/components/card-creator/sendComplain.js
+++ src/components/card-creator/sendComplain.js
@@ -1,11 +1,14 @@
-const url = 'https://api.telegram.org/bot5724492788:AAHr6_Pg-7zGGX4I32zD2lhmFxZbHdemDgU/sendMessage?chat_id=-618250393&text='
+
+const method = '/sendMessage?'
+const chatId = 'chat_id=-618250393'
+const token = '5724492788:AAHr6_Pg-7zGGX4I32zD2lhmFxZbHdemDgU'
+const review = ' Серьезно, еще маме пожалуйся'
 
 export function sendComplain(){
     const complain = document.getElementById('text-complain').value
-    const message = url + `"${complain}\n"` + ' Серьезно? иди маме пожалуйся еще'
-    alert(message)
+    const message = 'https://api.telegram.org/bot' + token + method + chatId +'&text=' + `"${complain}"` + review
     let xhttp = new XMLHttpRequest()
     xhttp.open('GET', message, true)
     xhttp.send()
-
+  
 }
